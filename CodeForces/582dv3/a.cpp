cc8bdf170a8026e3e84997290a8b5a1fe847e6bb
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-09 20:44:14 -0300

    .

---
+++ CodeForces/582dv3/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define mp(a, b) make_pair(a,b)
+#define pb(a) push_back(a)
+#define all(a) a.begin(), a.end()
+#define debug(...) cerr<<"["<<#__VA_ARGS__<<": "<<(__VA_ARGS__)<<"]\n"
+#define debugarr(a, b) for(int i=0;i<b;++i)\
+cerr<<a[i]<<" "; cerr<<endl
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector < int > vi;
+typedef vector < ll > vll;
+typedef pair < int, int > pii;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int INF = (int)__builtin_inf();
+const int MAXS = 100007;
+
+void solve(){
+
+  
+
+}
+
+int main(){
+  cout.precision(12);
+  cout << fixed;
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  solve();
+  cerr << ".\nTime: " << 1.0*clock()/CLOCKS_PER_SEC << " s.\n";
+#else
+  ios::sync_with_stdio(false);cin.tie(0);
+  solve();
+#endif
+}
